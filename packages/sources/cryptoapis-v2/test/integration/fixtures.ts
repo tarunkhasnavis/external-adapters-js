b44a937123c38bc9d681d7f68ba6378108f15af4
Austin Born <austinborn212@gmail.com>
2021-11-15 07:15:33 -0800

    test: cryptoapis-v2 integration tests (wip)

---
+++ packages/sources/cryptoapis-v2/test/integration/fixtures.ts
@@ -0,0 +1,13 @@
+import nock from 'nock'
+
+export function mockBalanceResponse() {
+  //TODO
+}
+
+export function mockBcInfoResponse() {
+  //TODO
+}
+
+export function mockCryptoResponse() {
+  //TODO
+}
